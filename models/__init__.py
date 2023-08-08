9e97d3400dec653ed84b4aca417b390405113853
xanderyeng <ckalex54@gmail.com>
2023-08-08 20:40:56 +0300

    Modified the base_model.py with a small correction

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 # models/__init__.py
 # I MAY NEED THESE ONLY IF I FIND A NEED
 # TO CREATE A BASEMODE FOR THEM ( INHERITANCE )
-# from .base_model import BaseModel
+
+
+from .base_model import BaseModel
 # from .user import User
 # from .state import State
 # Add more imports for other model classes

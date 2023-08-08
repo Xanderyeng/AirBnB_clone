671ce66a9b3c6af9687f45549b5133e33addca67
xanderyeng <ckalex54@gmail.com>
2023-08-08 17:25:39 +0300

    Added a test file

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# models/__init__.py
+# I MAY NEED THESE ONLY IF I FIND A NEED
+# TO CREATE A BASEMODE FOR THEM ( INHERITANCE )
+# from .base_model import BaseModel
+# from .user import User
+# from .state import State
+# Add more imports for other model classes

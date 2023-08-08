d771aabba99e2b85e0435fb91417489569282b72
xanderyeng <ckalex54@gmail.com>
2023-08-08 17:06:39 +0300

    pycodestyle checks successful

--- models/base_model.py
+++ models/base_model.py
@@ -16,16 +16,20 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the model."""
-    return (if"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+    return (if "[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-    """Returns a dictionary representation of the model."""
-        dict_ = {key: value for key, value in self.__dict__.items() if value is not None}
-        dict_["__class__"] = self.__class__.__name__
-        dict_["created_at"] = self.created_at.isoformat()
-        dict_["updated_at"] = self.updated_at.isoformat()
+        """Returns a dictionary representation of the model."""
+    dict_ = {
+            key: value
+            for key, value in self.__dict__.items()
+            if value is not None
+    }
+    dict_["__class__"] = self.__class__.__name__
+    dict_["created_at"] = self.created_at.isoformat()
+    dict_["updated_at"] = self.updated_at.isoformat()
     return dict_

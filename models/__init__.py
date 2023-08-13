4f41d05babd7271c21179acde14a10a789209500
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-08-13 11:24:17 -0700

    Solving out checker probs

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@
 # TO CREATE A BASEMODEL FOR THEM ( INHERITANCE )
 
 
-# from models.base_model import BaseModel
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
